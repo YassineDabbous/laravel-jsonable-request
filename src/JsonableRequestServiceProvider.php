baf0c71c3dca7f46b4bddedcadb76427c9cff2f5
yassine-dabbous <yassine.dabbous@gmail.com>
2026-02-17 11:10:56 +0000

    ServiceProvider

---
+++ src/JsonableRequestServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace YassineDabbous\JsonableRequest;
+
+use Illuminate\Support\ServiceProvider;
+
+class JsonableRequestServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+}
